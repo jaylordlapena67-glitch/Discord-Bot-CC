c2076a6609df01646d42f44cccd94763daa38ad0
jaytest011020-dot <jaytest011020@gmail.com>
2025-10-14 09:26:26 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  botName: "Jaylord Bot",          // Hardcoded bot name
+  token: process.env.BOT_TOKEN,    // Discord bot token from Render
+  prefix: "-",                     // optional command prefix
+  clientId: "", // hardcoded client ID
+  testGuildId: "",                 // optional test guild ID
+  autorestart: 40                  // optional auto-restart interval
+};
