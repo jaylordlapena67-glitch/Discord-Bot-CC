d236517a3764ceec432dcbd6aa899669912ec737
jaytest011020-dot <jaytest011020@gmail.com>
2025-10-14 09:27:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   botName: "Jaylord Bot",          // Hardcoded bot name
   token: process.env.BOT_TOKEN,    // Discord bot token from Render
-  prefix: "-",                     // optional command prefix
-  clientId: "", // hardcoded client ID
+  prefix: "/",                     // optional command prefix
+  clientId: "1426907191033597982", // hardcoded client ID
   testGuildId: "",                 // optional test guild ID
   autorestart: 40                  // optional auto-restart interval
 };
